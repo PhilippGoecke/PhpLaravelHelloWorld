91520b602a836a561dbd9ab7f4809c98d61ea027
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2024-04-30 23:25:20 +0200

    Update docker-run-laravel.bash

--- docker-run-laravel.bash
+++ docker-run-laravel.bash
@@ -1,3 +1,3 @@
 docker build --no-cache --rm -f Containerfile -t laravel:demo .
 docker run --interactive --tty -p 8080:8080 laravel:demo
-echo "browse http://localhost:8080/"
+echo "browse http://localhost:8080/hello"
