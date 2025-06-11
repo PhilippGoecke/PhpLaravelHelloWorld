d26a71e17811b73a81c2e589df9d01b4adac87e9
PhilippGoecke <81357365+PhilippGoecke@users.noreply.github.com>
2025-06-11 23:39:45 +0200

    Update podman-run-laravel.bash

--- podman-run-laravel.bash
+++ podman-run-laravel.bash
@@ -1,3 +1,3 @@
 podman build --no-cache --rm -f Containerfile -t laravel:demo .
 podman run --interactive --tty -p 8080:8080 laravel:demo
-echo "browse http://localhost:8080/hello/World"
+echo "browse http://localhost:8080/hello?name=World"
